6e6899640c0577ca2cc4fcecd1b17245dd2b0595
Yanliang Han <skywalkerhunter@gmail.com>
2016-06-15 00:23:16 +0800

    add jump

--- src/main/java/com/oliver/DynamicProgramming/Jump.java
+++ src/main/java/com/oliver/DynamicProgramming/Jump.java
@@ -5,50 +5,35 @@ package com.oliver.DynamicProgramming;
  */
 public class Jump {
     public static void main(String[] args) {
-        testCanJump();
+        testJump();
     }
 
-    private static void testCanJump() {
-        System.out.println(canJump(new int[]{2,3,1,1,4}));
-        System.out.println(canJump(new int[]{3,2,1,0,4}));
+    private static void testJump() {
+        System.out.println(jump(new int[]{2,3,1,1,4}));
+        System.out.println(jump(new int[]{3,2,1,0,4}));
     }
 
-    public static boolean canJump(int[] A) {
+    public static int jump(int[] A) {
         if (A == null || A.length < 1) {
-            return true;
+            return 0;
         }
 
         int len = A.length;
-        int furthest = 0;
+        int[] jump = new int[len];
+        for (int i = 0; i < len; i++) {
+            jump[i] = Integer.MAX_VALUE;
+        }
+        jump[0] = 0;
 
         for (int i = 0; i < len; i++) {
-            if (i > furthest) {
-                break;
-            }
-            furthest = Math.max(furthest, i + A[i]);
-        }
-
-        return furthest >= len - 1;
-    }
-
-    public static boolean canJumpDP(int[] A) {
-        if (A == null || A.length < 1) {
-            return true;
-        }
-
-        int len = A.length;
-        boolean[] f = new boolean[len];
-        f[0] = true;
-
-        for (int i = 0; i < len; i++) {
-            if (!f[i]) {
+            if (jump[i] == Integer.MAX_VALUE) {
                 break;
             }
             for (int j = 1; j <= A[i] && i + j < len; j++) {
-                f[i + j] = true;
+                jump[i + j] = Math.min(jump[i + j], jump[i] + 1);
             }
         }
 
-        return f[len - 1];
+        return jump[len - 1];
     }
 }
