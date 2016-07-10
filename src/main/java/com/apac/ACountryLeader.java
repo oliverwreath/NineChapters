88f30025526180b953e0cba63add01178d7565b0
oliver.han <skywalkerhunter@gmail.com>
2016-07-10 13:53:30 +0800

    add [Google APAC 2017] ACountryLeader

---
+++ src/main/java/com/apac/ACountryLeader.java
@@ -0,0 +1,51 @@
+package com.apac;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.apac.Util.getPrintWriter;
+
+/**
+ * Created by yanli_000 on 16/7/10.
+ */
+public class ACountryLeader {
+    private final static Logger logger = LoggerFactory.getLogger(ACountryLeader.class);
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        new ACountryLeader().go();
+        logger.info("time taken = {}s", (System.currentTimeMillis() - startTime) / 1000.0);
+    }
+
+    private void go() {
+        Scanner scanner = new Scanner(System.in);
+        int numOfCasesT;
+        if (scanner.hasNext()) {
+            numOfCasesT = scanner.nextInt();
+            PrintWriter printWriter = getPrintWriter("C:\\Users\\yanli_000\\Desktop\\ACountryLeader.out");
+
+            for (int caseI = 0; caseI < numOfCasesT; caseI++) {
+                int numOfLeadersN = scanner.nextInt();
+                scanner.nextLine();
+                logger.info("numOfLeadersN = {}", numOfLeadersN);
+                ArrayList<Util.LeaderEntry> leaderEntries = new ArrayList<Util.LeaderEntry>();
+                for (int i = 0; i < numOfLeadersN; i++) {
+                    leaderEntries.add(new Util.LeaderEntry(scanner.nextLine()));
+                }
+                logger.info("before sort = {}", leaderEntries);
+                Collections.sort(leaderEntries);
+                logger.info("after sort = {}", leaderEntries);
+                String result = "Case #" + (caseI + 1) + ": " + leaderEntries.get(0);
+//                System.out.println(result);
+                printWriter.println(result);
+            }
+
+            printWriter.flush();
+        }
+    }
+}
--- src/main/java/com/apac/Util.java
+++ src/main/java/com/apac/Util.java
@@ -3,8 +3,12 @@ package com.apac;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
 import com.google.common.base.Throwables;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +17,110 @@ import org.slf4j.LoggerFactory;
  */
 public class Util {
     private final static Logger logger = LoggerFactory.getLogger(Util.class);
+
+    public static void main(String[] args) {
+        new Util().testLeaderRanking3();
+    }
+
+    public static class LeaderEntry implements Comparable {
+        String name;
+        int distinctCount;
+
+        public LeaderEntry(String name) {
+            this.name = name;
+            this.distinctCount = getDistinctAlphabetCount(name);
+        }
+
+        public LeaderEntry(String name, int distinctCount) {
+            this.name = name;
+            this.distinctCount = distinctCount;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+//        @Override
+//        public String toString() {
+//            return "LeaderEntry{" +
+//                    "name='" + name + '\'' +
+//                    ", distinctCount=" + distinctCount +
+//                    '}';
+//        }
+
+        public int compareTo(Object o) {
+            if (o == null) {
+                return 0;
+            }
+            if (o instanceof LeaderEntry) {
+                LeaderEntry anotherLeaderEntry = (LeaderEntry) o;
+                if (this.distinctCount == anotherLeaderEntry.distinctCount) {
+                    return this.name.compareTo(anotherLeaderEntry.name);
+                } else {
+                    return anotherLeaderEntry.distinctCount - this.distinctCount;
+                }
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public void testLeaderRanking() {
+        ArrayList<LeaderEntry> leaderEntries = new ArrayList<LeaderEntry>();
+        leaderEntries.add(new LeaderEntry("ADAM"));
+        leaderEntries.add(new LeaderEntry("BOB"));
+        leaderEntries.add(new LeaderEntry("JOHNSON"));
+        logger.info("before sort = {}", leaderEntries);
+        Collections.sort(leaderEntries);
+        logger.info("after sort = {}", leaderEntries);
+    }
+
+    public void testLeaderRanking2() {
+        ArrayList<LeaderEntry> leaderEntries = new ArrayList<LeaderEntry>();
+        leaderEntries.add(new LeaderEntry("A AB C"));
+        leaderEntries.add(new LeaderEntry("DEF"));
+        logger.info("before sort = {}", leaderEntries);
+        Collections.sort(leaderEntries);
+        logger.info("after sort = {}", leaderEntries);
+    }
+
+    public void testLeaderRanking3() {
+        ArrayList<LeaderEntry> leaderEntries = new ArrayList<LeaderEntry>();
+        leaderEntries.add(new LeaderEntry("ADAM"));
+        leaderEntries.add(new LeaderEntry("BOB"));
+        leaderEntries.add(new LeaderEntry("JOHNSON"));
+        leaderEntries.add(new LeaderEntry("A AB C"));
+        leaderEntries.add(new LeaderEntry("DEF"));
+        logger.info("before sort = {}", leaderEntries);
+        Collections.sort(leaderEntries);
+        logger.info("after sort = {}", leaderEntries);
+    }
+
+    public void testGetDistinctAlphabetCount() {
+        System.out.println(new LeaderEntry("ADAM"));
+        System.out.println(new LeaderEntry("BOB"));
+        System.out.println(new LeaderEntry("JOHNSON"));
+        System.out.println(new LeaderEntry("A AB C"));
+        System.out.println(new LeaderEntry("DEF"));
+    }
+
+    public static int getDistinctAlphabetCount(String name) {
+        if (StringUtils.isBlank(name)) {
+            return 0;
+        }
+
+        int len = name.length();
+        HashSet<Character> tmpSet = new HashSet<Character>();
+        for (int i = 0; i < len; i++) {
+            char oneChar = name.charAt(i);
+            if (oneChar != ' ') {
+                tmpSet.add(oneChar);
+            }
+        }
+
+        return tmpSet.size();
+    }
 
     public static PrintWriter getPrintWriter(String filePath) {
         File file = new File(filePath);
