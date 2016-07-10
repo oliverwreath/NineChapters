9ea3ebda60b743bb60f21136551f47a6c581add3
oliver.han <skywalkerhunter@gmail.com>
2016-07-10 17:48:57 +0800

    [JiuZhang] deleteDuplicates

--- src/main/java/com/oliver/LinkedList/DeleteDuplicates.java
+++ src/main/java/com/oliver/LinkedList/DeleteDuplicates.java
@@ -1,68 +1,25 @@
 package com.oliver.LinkedList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Created by Yanliang Han on 2016/6/26.
+ * Created by yanli_000 on 16/7/10.
  */
 public class DeleteDuplicates {
+    private final static Logger logger = LoggerFactory.getLogger(DeleteDuplicates.class);
 
     public static void main(String[] args) {
         testDeleteDuplicates();
     }
 
-    public static void testDeleteDuplicates() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(1);
-        head.next.next = new ListNode(2);
-        head.next.next.next = new ListNode(2);
-        head.next.next.next.next = new ListNode(2);
-        head.next.next.next.next.next = new ListNode(2);
-        head.next.next.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next.next.next.next = new ListNode(5);
-
-        System.out.println(deleteDuplicates(head));
+    private static void testDeleteDuplicates() {
+        long startTime = System.currentTimeMillis();
+        deleteDuplicates();
+        logger.info("time taken = {}s", (System.currentTimeMillis() - startTime) / 1000.0);
     }
 
-    public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+    private static void deleteDuplicates() {
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode slow = dummy;
-        ListNode fast = head;
-        while (fast != null) {
-            if (fast.next != null && fast.val == fast.next.val) {
-                int target = fast.val;
-                while (fast != null && fast.val == target) {
-                    fast = fast.next;
-                }
-                slow.next = fast;
-            } else {
-                slow = fast;
-                fast = fast.next;
-            }
-        }
-
-        return dummy.next;
-    }
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-
-        @Override
-        public String toString() {
-            return "ListNode{" +
-                    "val=" + val +
-                    ", next=" + next +
-                    '}';
-        }
     }
 }
