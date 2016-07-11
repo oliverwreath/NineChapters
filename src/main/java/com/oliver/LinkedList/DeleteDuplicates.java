45da98ba991c31148736ec87296262f258ba06e8
Yanliang Han <skywalkerhunter@gmail.com>
2016-07-12 03:03:47 +0800

    [JiuZhang] DeleteDuplicates

--- src/main/java/com/oliver/LinkedList/DeleteDuplicates.java
+++ src/main/java/com/oliver/LinkedList/DeleteDuplicates.java
@@ -10,17 +10,65 @@ public class DeleteDuplicates {
     private final static Logger logger = LoggerFactory.getLogger(DeleteDuplicates.class);
 
     public static void main(String[] args) {
-        testDeleteDuplicates();
-    }
-
-    private static void testDeleteDuplicates() {
         long startTime = System.currentTimeMillis();
-        deleteDuplicates();
+        testDeleteDuplicates();
         logger.info("time taken = {}s", (System.currentTimeMillis() - startTime) / 1000.0);
     }
 
-    private static void deleteDuplicates() {
+    private static void testDeleteDuplicates() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next = new ListNode(5);
+        ListNode resultHead = deleteDuplicates(head);
+        logger.info("{}", resultHead);
 
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(1);
+        head2.next.next = new ListNode(1);
+        head2.next.next.next = new ListNode(2);
+        head2.next.next.next.next = new ListNode(3);
+        ListNode resultHead2 = deleteDuplicates(head2);
+        logger.info("{}", resultHead2);
+
+        ListNode head3 = new ListNode(1);
+        ListNode resultHead3 = deleteDuplicates(head3);
+        logger.info("{}", resultHead3);
+
+        ListNode resultHead4 = deleteDuplicates(null);
+        logger.info("{}", resultHead4);
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy.next;
+
+        while (fast != null && fast.next != null) {
+            if (fast.val == fast.next.val) {
+                // duplication detected
+                fast = fast.next;
+                while (fast.next != null && fast.val == fast.next.val) {
+                    fast = fast.next;
+                }
+                fast = fast.next;
+                slow.next = fast;
+            } else {
+                // unique element
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+
+        return dummy.next;
     }
 
     public static class ListNode {
