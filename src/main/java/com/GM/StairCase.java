4b692b7a1bdc57ee05a435b51dc38e030109028b
Yanliang Han <skywalkerhunter@gmail.com>
2016-08-28 01:00:56 -0400

    add to GM.

---
+++ src/main/java/com/GM/StairCase.java
@@ -0,0 +1,38 @@
+package com.GM;
+
+/**
+ * Created by yanli on 8/27/2016.
+ */
+public class StairCase {
+    public static void main(String[] args) {
+        testStairCase(-1);
+        testStairCase(0);
+        testStairCase(1);
+        testStairCase(3);
+        testStairCase(6);
+    }
+
+    static void testStairCase(int n) {
+        StairCase(n);
+    }
+
+    static void StairCase(int n) {
+        if (n < 1) {
+            return;
+        }
+
+        final String symbol = "#";
+
+        for (int i = 1; i <= n; i++) {
+            String spaceString = "";
+            String symbolString = "";
+            for (int j = 0; j < n - i; j++) {
+                spaceString += " ";
+            }
+            for (int j = 0; j < i; j++) {
+                symbolString += symbol;
+            }
+            System.out.println(spaceString + symbolString);
+        }
+    }
+}
---
+++ src/main/java/com/GM/SumThemAll.java
@@ -0,0 +1,31 @@
+package com.GM;
+
+import java.util.Scanner;
+
+/**
+ * Created by yanli on 8/27/2016.
+ */
+public class SumThemAll {
+    public static void main(String[] args) {
+        testSum(new int[]{1, 2, 3, 4, 5});
+        testSum(new int[]{12, 12});
+        testSum(null);
+    }
+
+    static void testSum(int[] numbers) {
+        System.out.println(sum(numbers));
+    }
+
+    static int sum(int[] numbers) {
+        if (numbers == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
