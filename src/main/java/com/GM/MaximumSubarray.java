89945e000a453e5a610875ade24ad1ce73c5fd6d
Yanliang Han <skywalkerhunter@gmail.com>
2016-08-30 18:15:36 -0400

    add MaximumSubarray.

---
+++ src/main/java/com/GM/MaximumSubarray.java
@@ -0,0 +1,67 @@
+package com.GM;
+
+import java.util.Scanner;
+
+/**
+ * Created by yanli on 8/30/2016.
+ */
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int caseI = 0; caseI < t; caseI++) {
+            int n = scan.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scan.nextInt();
+            }
+            maximumSub(a);
+        }
+    }
+
+    static void testMaximumSubarray() {
+//        maximumSubarray();
+    }
+
+    static void maximumSub(int[] a) {
+        int len = a.length;
+        int max = a[0];
+        int maxIndex = 0;
+        for (int i = 1; i < len; i++) {
+            if (a[i] > max) {
+                max = a[i];
+                maxIndex = i;
+            }
+        }
+
+        maximumSubarray(a, len, max, maxIndex);
+        maximumSubsequence(a, len, max, maxIndex);
+    }
+
+    static void maximumSubarray(int[] a, int len, int max, int maxIndex) {
+        int maxSum = max;
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += a[i];
+            maxSum = Math.max(maxSum, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.format("%d ", maxSum);
+    }
+
+    static void maximumSubsequence(int[] a, int len, int max, int maxIndex) {
+        int maxSum = max;
+        for (int i = 0; i < len; i++) {
+            if (maxIndex != i) {
+                if (a[i] > 0) {
+                    maxSum += a[i];
+                }
+            }
+        }
+
+        System.out.println(maxSum);
+    }
+}
