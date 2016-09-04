90bdd168263fad65521b94014d8e42354c9921cd
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-04 18:17:43 -0400

    add CountOnes.

---
+++ src/main/java/com/lintCode/Book_Coding_Interviews/CountOnes.java
@@ -0,0 +1,30 @@
+package com.lintCode.Book_Coding_Interviews;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 9/4/2016.
+ */
+public class CountOnes {
+    private final static Logger logger = LoggerFactory.getLogger(CountOnes.class);
+
+    public static void main(String[] arg) {
+        testCountOnes();
+    }
+
+    private static void testCountOnes() {
+        logger.info("{}", countOnes(1023));
+        logger.info("{}", countOnes(32));
+        logger.info("{}", countOnes(5));
+    }
+
+    static int countOnes(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
+    }
+}
