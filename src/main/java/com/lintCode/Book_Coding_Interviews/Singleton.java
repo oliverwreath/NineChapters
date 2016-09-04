68e7e23ccdb0e0fc0d474153433e601787795636
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-04 17:50:02 -0400

    add Singleton.

---
+++ src/main/java/com/lintCode/Book_Coding_Interviews/Singleton.java
@@ -0,0 +1,25 @@
+package com.lintCode.Book_Coding_Interviews;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 9/4/2016.
+ */
+public class Singleton {
+    private final static Logger logger = LoggerFactory.getLogger(Singleton.class);
+
+    private final static Singleton solution = new Singleton();
+
+    public static void main(String[] arg) {
+        testSingleton();
+    }
+
+    private static void testSingleton() {
+        logger.info("{}", getInstance());
+    }
+
+    static Singleton getInstance() {
+        return solution;
+    }
+}
