92095b8620399a92711a570ff27186dad8c0c2fc
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-04 17:44:36 -0400

    add Fibonacci.

---
+++ src/main/java/com/lintCode/Book_Coding_Interviews/Fibonacci.java
@@ -0,0 +1,44 @@
+package com.lintCode.Book_Coding_Interviews;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 9/4/2016.
+ */
+public class Fibonacci {
+    private final static Logger logger = LoggerFactory.getLogger(Fibonacci.class);
+
+    public static void main(String[] arg) {
+        testFibonacci();
+    }
+
+    private static void testFibonacci() {
+        logger.info("{}", fibonacci(10));
+    }
+
+    static int[] fib = new int[1999];
+    static int index = 1;
+
+    static {
+        fib[1] = 1;
+    }
+
+    static int fibonacci(int n) {
+        // filter invalid input
+        if (n < 0) {
+            return -1;
+        }
+
+        // check if we need to update
+        if (n - 1 > index) {
+            for (int i = index + 1; i < n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            index = n - 1;
+        }
+
+        // get result from fib[n - 1]
+        return fib[n - 1];
+    }
+}
