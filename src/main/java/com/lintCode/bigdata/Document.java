3969573107bfa65f469c01ea6883c00786654dc4
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-10 20:00:31 -0400

    add InvertedIndex.

---
+++ src/main/java/com/lintCode/bigdata/Document.java
@@ -0,0 +1,14 @@
+package com.lintCode.bigdata;
+
+/**
+ * Created by yanli on 9/10/2016.
+ */
+class Document {
+    public int id;
+    public String content;
+
+    public Document(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
