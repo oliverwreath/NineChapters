f7bb69bbb039968b66001e22a2b8159c4b981514
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-11 14:56:36 -0400

    update SubarraySumClosest.

--- src/main/java/com/lintCode/ArrayAndNumbers/SubarraySumClosest.java
+++ src/main/java/com/lintCode/ArrayAndNumbers/SubarraySumClosest.java
@@ -31,38 +31,30 @@ public class SubarraySumClosest {
 
         // get sum
         int len = nums.length;
-        Pair[] sumArray = new Pair[len];
-        sumArray[0] = new Pair(nums[0], 0);
-        for (int i = 1; i < len; i++) {
-            sumArray[i] = new Pair(sumArray[i - 1].sum + nums[i], i);
+        Pair[] sumArray = new Pair[len + 1];
+        sumArray[0] = new Pair(0, 0);
+        for (int i = 1; i <= len; i++) {
+            sumArray[i] = new Pair(sumArray[i - 1].sum + nums[i - 1], i);
         }
 
         // sort
-//        System.out.println(Arrays.toString(sumArray));
+//        logger.info("sumArray = {}", Arrays.toString(sumArray));
         Arrays.sort(sumArray, new Comparator<Pair>() {
             public int compare(Pair o1, Pair o2) {
                 return o1.sum - o2.sum;
             }
         });
-//        System.out.println(Arrays.toString(sumArray));
+//        logger.info("sumArray = {}", Arrays.toString(sumArray));
 
         // minimum adjacent diff (closest to 0 subarray)
         int minDiff = Integer.MAX_VALUE;
         int[] closestRet = new int[2];
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i <= len; i++) {
             int diff = sumArray[i].sum - sumArray[i - 1].sum;
             if (diff < minDiff) {
                 minDiff = diff;
-                closestRet[0] = sumArray[i].index;
-                closestRet[1] = sumArray[i - 1].index;
-            }
-        }
-        for (int i = 0; i < len; i++) {
-            int diff = Math.abs(sumArray[i].sum);
-            if (diff < minDiff) {
-                minDiff = diff;
-                closestRet[0] = sumArray[i].index;
-                closestRet[1] = sumArray[i].index;
+                closestRet[0] = sumArray[i].index - 1;
+                closestRet[1] = sumArray[i - 1].index - 1;
             }
         }
 
