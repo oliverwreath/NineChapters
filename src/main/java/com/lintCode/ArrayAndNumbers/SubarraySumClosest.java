cc016dcb1a038c93b9c25f64b647a599555f80e1
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-11 14:32:26 -0400

    update SubarraySumClosest, time complexity O(nlogn)

--- src/main/java/com/lintCode/ArrayAndNumbers/SubarraySumClosest.java
+++ src/main/java/com/lintCode/ArrayAndNumbers/SubarraySumClosest.java
@@ -1,5 +1,8 @@
 package com.lintCode.ArrayAndNumbers;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +17,13 @@ public class SubarraySumClosest {
     }
 
     private static void testSubarraySumClosest() {
-        logger.info("{}", subarraySumClosest(new int[]{-3, 1, 1, -3, 5}));
+        SubarraySumClosest subarraySumClosest = new SubarraySumClosest();
+        logger.info("{}", subarraySumClosest.subarraySumClosest(new int[]{-3, 1, 1, -3, 5}));
+        logger.info("{}", subarraySumClosest.subarraySumClosest(new int[]{2147483647}));
+        logger.info("{}", subarraySumClosest.subarraySumClosest(new int[]{-3, 1, 0, -3, 5}));
     }
 
-    static int[] subarraySumClosest(int[] nums) {
+    public int[] subarraySumClosest(int[] nums) {
         // filter abnormal inputs
         if (nums == null || nums.length == 0) {
             return new int[]{};
@@ -25,34 +31,64 @@ public class SubarraySumClosest {
 
         // get sum
         int len = nums.length;
-        int[] sumArray = new int[len + 1];
-        {
-            int tmpSum = 0;
-            for (int i = len - 1; i >= 0; i--) {
-                tmpSum += nums[i];
-                sumArray[i] = tmpSum;
-            }
+        Pair[] sumArray = new Pair[len];
+        sumArray[0] = new Pair(nums[0], 0);
+        for (int i = 1; i < len; i++) {
+            sumArray[i] = new Pair(sumArray[i - 1].sum + nums[i], i);
         }
 
-        // two fors
-        int closestSum = Integer.MAX_VALUE;
-        int[] closestRet = new int[]{0, 0};
+        // sort
+//        System.out.println(Arrays.toString(sumArray));
+        Arrays.sort(sumArray, new Comparator<Pair>() {
+            public int compare(Pair o1, Pair o2) {
+                return o1.sum - o2.sum;
+            }
+        });
+//        System.out.println(Arrays.toString(sumArray));
+
+        // minimum adjacent diff (closest to 0 subarray)
+        int minDiff = Integer.MAX_VALUE;
+        int[] closestRet = new int[2];
+        for (int i = 1; i < len; i++) {
+            int diff = sumArray[i].sum - sumArray[i - 1].sum;
+            if (diff < minDiff) {
+                minDiff = diff;
+                closestRet[0] = sumArray[i].index;
+                closestRet[1] = sumArray[i - 1].index;
+            }
+        }
         for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                int tmpSum = sumArray[i] - sumArray[j + 1];
-                if (ACloserThenB(tmpSum, closestSum)) {
-                    closestRet[0] = i;
-                    closestRet[1] = j;
-                    closestSum = tmpSum;
-                }
+            int diff = Math.abs(sumArray[i].sum);
+            if (diff < minDiff) {
+                minDiff = diff;
+                closestRet[0] = sumArray[i].index;
+                closestRet[1] = sumArray[i].index;
             }
         }
 
         // retrun result
+        Arrays.sort(closestRet);
+        if (closestRet[0] < closestRet[1]) {
+            closestRet[0]++;
+        }
         return closestRet;
     }
 
-    static boolean ACloserThenB(int a, int b) {
-        return Math.abs(a) < Math.abs(b);
+    private class Pair {
+        private int sum;
+        private int index;
+
+        public Pair(int sum, int index) {
+            this.sum = sum;
+            this.index = index;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "sum=" + sum +
+                    ", index=" + index +
+                    '}';
+        }
     }
 }
