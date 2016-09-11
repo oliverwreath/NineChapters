8a3ae9e329211ae6fc86a47d625c6d6d945228d0
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-10 23:53:12 -0400

    update integerReplacement.

--- src/main/java/com/LeetCode/IntegerReplacement.java
+++ src/main/java/com/LeetCode/IntegerReplacement.java
@@ -17,12 +17,12 @@ public class IntegerReplacement {
 //        logger.info("{}", integerReplacement(8));//3
 //        logger.info("{}", integerReplacement(7));//4
 //        logger.info("{}", integerReplacement(3));//2
-        for (int i = 0; i < 20000; i++) {
+        for (int i = 2147413647; i <= 2147483647; i++) {
             logger.info("integerReplacement({}) = {}", i, integerReplacement(i));
         }
     }
 
-    static final int LEN = 100000002;
+    static final int LEN = 2147483640;
     static byte[] min = new byte[LEN];
 
     static {
@@ -38,17 +38,19 @@ public class IntegerReplacement {
             return Integer.MAX_VALUE;
         }
 
-        if (min[n] < 0) {
+        if (n < LEN && min[n] >= 0) {
+            return (int) min[n];
+        } else {
             byte counter;
             if (n % 2 == 0) {
                 counter = (byte) (1 + integerReplacement(n / 2));
             } else {
                 counter = (byte) (1 + Math.min(integerReplacement(n - 1), integerReplacement(n + 1)));
             }
-            min[n] = counter;
+            if (n < LEN) {
+                min[n] = counter;
+            }
             return counter;
-        } else {
-            return (int) min[n];
         }
     }
 }
