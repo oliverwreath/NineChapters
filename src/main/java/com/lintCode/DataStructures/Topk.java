1b174155afb91dbddf0b4d7c5a68035e384acb7a
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-13 20:21:37 -0400

    add Topk largest numbers.

---
+++ src/main/java/com/lintCode/DataStructures/Topk.java
@@ -0,0 +1,43 @@
+package com.lintCode.DataStructures;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 9/13/2016.
+ */
+public class Topk {
+    private final static Logger logger = LoggerFactory.getLogger(Topk.class);
+
+    public static void main(String[] arg) {
+        testTopk();
+    }
+
+    private static void testTopk() {
+        logger.info("{}", topk(new int[]{3, 10, 1000, -99, 4, 100}, 3));
+    }
+
+    static int[] topk(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>(k, Collections.<Integer>reverseOrder());
+        for (int i = 0; i < nums.length; i++) {
+            heap.add(nums[i]);
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; i++) {
+            ret[i] = heap.poll();
+        }
+//        Arrays.sort(nums);
+//        int[] ret = new int[k];
+//        for (int i = 0; i < k; i++) {
+//            ret[i] = nums[nums.length - 1 - i];
+//        }
+        return ret;
+    }
+}
