c46a81007576e128192116d3169ab3ce635d2148
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-17 16:12:35 -0400

    add BinaryNumbers.

---
+++ src/main/java/com/HackerRank/BinaryNumbers.java
@@ -0,0 +1,37 @@
+package com.HackerRank;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 9/16/2016.
+ */
+public class BinaryNumbers {
+    private final static Logger logger = LoggerFactory.getLogger(BinaryNumbers.class);
+
+    public static void main(String[] arg) {
+        testBinaryNumbers();
+    }
+
+    private static void testBinaryNumbers() {
+        binaryNumbers();
+    }
+
+    static void binaryNumbers() {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+        int n = 439;
+        int counter = 0;
+        int maxCounter = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                counter++;
+                maxCounter = Math.max(maxCounter, counter);
+            } else {
+                counter = 0;
+            }
+            n /= 2;
+        }
+        System.out.println(maxCounter);
+    }
+}
