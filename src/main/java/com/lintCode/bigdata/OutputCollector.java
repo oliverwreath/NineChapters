cea80737a107670140610a8990c6cbbec469851e
Yanliang Han <skywalkerhunter@gmail.com>
2016-09-25 21:18:42 -0400

    add TopKFrequentWords

--- src/main/java/com/lintCode/bigdata/OutputCollector.java
+++ src/main/java/com/lintCode/bigdata/OutputCollector.java
@@ -1,12 +1,28 @@
 package com.lintCode.bigdata;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by yanli on 9/10/2016.
  */
 class OutputCollector<K, V> {
-    public void collect(K key, V value) {
+    private LinkedHashMap<K, List<V>> outputBuffer = new LinkedHashMap<K, List<V>>();
 
-    }
     // Adds a key/value pair to the output buffer
+    public void collect(K key, V value) {
+        if (outputBuffer.containsKey(key)) {
+            outputBuffer.get(key).add(value);
+        } else {
+            LinkedList<V> list = new LinkedList<V>();
+            list.add(value);
+            outputBuffer.put(key, list);
+        }
+    }
+
+    public LinkedHashMap<K, List<V>> getOutputBuffer() {
+        return outputBuffer;
+    }
 }
 
