69731cf5434d11e5816bb56d391778ff209768bc
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-01 22:04:34 -0400

    udpate. verified the length need to be same.

--- src/main/java/com/LeetCode/Weekly7/ValidWordAbbreviation_408.java
+++ src/main/java/com/LeetCode/Weekly7/ValidWordAbbreviation_408.java
@@ -32,29 +32,37 @@ public class ValidWordAbbreviation_408 {
         int n = word.length();
         int m = abr.length();
         int i = 0;
-        for (int j = 0; j < m; j++) {
+        int j = 0;
+        for (; j < m; j++) {
             if (i >= n) {
                 return false;
             }
 //            System.out.println(i + " " + j + "; " + word.charAt(i) + " " + abr.charAt(j));
             char currentAbr = abr.charAt(j);
             if (Character.isDigit(currentAbr)) {
+                if (currentAbr == '0') {
+                    return false;
+                }
                 int number = currentAbr - '0';
                 while (j + 1 < m && Character.isDigit(abr.charAt(j + 1))) {
+                    if (abr.charAt(j + 1) == '0') {
+                        return false;
+                    }
                     number *= 10;
                     number += abr.charAt(j + 1) - '0';
                     j++;
                 }
                 i += number;
-                if (i > n) {
-                    return false;
-                }
             } else {
                 if (word.charAt(i) != currentAbr) {
                     return false;
                 }
                 i++;
             }
+        }
+//        System.out.println("i = " + i + "; j = " + j + "; n = " + n);
+        if (i > n) {
+            return false;
         }
 
         return true;
