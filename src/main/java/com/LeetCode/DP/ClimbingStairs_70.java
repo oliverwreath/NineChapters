2e57dc36d29f625f3b174ec7bb4c68e9ca3c79af
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-04 22:42:45 -0400

    add 70. Climbing Stairs. DP. 1D. Simple.

---
+++ src/main/java/com/LeetCode/DP/ClimbingStairs_70.java
@@ -0,0 +1,41 @@
+package com.LeetCode.DP;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 2016-10-04.
+ */
+public class ClimbingStairs_70 {
+    private final static Logger logger = LoggerFactory.getLogger(ClimbingStairs_70.class);
+
+    public static void main(String[] arg) {
+        new ClimbingStairs_70().testHouseRobber_198();
+    }
+
+    private void testHouseRobber_198() {
+        logger.info("{}", climbStairs(-1));
+        logger.info("{}", climbStairs(0));
+        logger.info("{}", climbStairs(1));
+        logger.info("{}", climbStairs(2));
+        logger.info("{}", climbStairs(3));
+    }
+
+    public int climbStairs(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
+    }
+}
