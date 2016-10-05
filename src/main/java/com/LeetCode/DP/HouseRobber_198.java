5ccf2260df5f6b5ee86e1fcfb0fd45fb27c69700
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-04 22:35:23 -0400

    add 198. House Robber. DP.

---
+++ src/main/java/com/LeetCode/DP/HouseRobber_198.java
@@ -0,0 +1,41 @@
+package com.LeetCode.DP;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 2016-10-04.
+ */
+public class HouseRobber_198 {
+    private final static Logger logger = LoggerFactory.getLogger(HouseRobber_198.class);
+
+    public static void main(String[] arg) {
+        new HouseRobber_198().testHouseRobber_198();
+    }
+
+    private void testHouseRobber_198() {
+        logger.info("{}", rob(new int[]{2, 5, 1}));
+    }
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        long answer = 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+//        int max = Math.max(dp[0], dp[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+//            max = Math.max(max, dp[i]);
+        }
+
+        return dp[n - 1];
+    }
+}
