a2617a42974b107ef08b45153367dbcb3336b8f1
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-14 19:36:34 -0400

    add kick-ass DataStructure, UnionFind.

---
+++ src/main/java/com/lintCode/Advanced/UnionFind/UnionFind.java
@@ -0,0 +1,50 @@
+package com.lintCode.Advanced.UnionFind;
+
+import java.util.HashMap;
+
+/**
+ * Created by yanli on 2016-10-14.
+ */
+class UnionFind {
+    HashMap<Integer, Integer> father = new HashMap<Integer, Integer>();
+
+    public UnionFind(int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int id = converttoId(i, j, m);
+                father.put(id, id);
+            }
+        }
+    }
+
+    int compressed_find(int key) {
+        // find
+        int parent = father.get(key);
+        while (parent != father.get(parent)) {
+            parent = father.get(parent);
+        }
+
+        // compress
+        int temp = -1;
+        int fa = key;
+        while (fa != father.get(fa)) {
+            temp = father.get(fa);
+            father.put(fa, parent);
+            fa = temp;
+        }
+        return parent;
+
+    }
+
+    void union(int key, int keyBigger) {
+        int boss = compressed_find(key);
+        int bossBigger = compressed_find(keyBigger);
+        if (boss != bossBigger) {
+            father.put(boss, bossBigger);
+        }
+    }
+
+    int converttoId(int x, int y, int m) {
+        return x * m + y;
+    }
+}
