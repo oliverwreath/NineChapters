5f44d6b5397fc1808f8131135c831b2ff4d56741
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-15 10:15:46 -0400

    add ViralAdvertising

---
+++ src/main/java/com/HackerRank/OpenBracketCodeSprint/ViralAdvertising.java
@@ -0,0 +1,26 @@
+package com.HackerRank.OpenBracketCodeSprint;
+
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 2016-10-15.
+ */
+public class ViralAdvertising {
+    private final static Logger logger = LoggerFactory.getLogger(ViralAdvertising.class);
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int current = 2;
+        long sum = current;
+        for (int i = n; i > 1; i--) {
+            current = current * 3 / 2;
+            sum += current;
+        }
+        System.out.println(sum);
+    }
+}
