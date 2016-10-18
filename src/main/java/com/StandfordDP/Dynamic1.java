0abbf11cacdb15369c1986d3fa87d255e661e32a
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-18 15:02:16 -0400

    add Dynamic1. Stanford - introduction to Dynamic Programming.

---
+++ src/main/java/com/StandfordDP/Dynamic1.java
@@ -0,0 +1,46 @@
+package com.StandfordDP;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 2016-09-26.
+ */
+public class Dynamic1 {
+    private final static Logger logger = LoggerFactory.getLogger(Dynamic1.class);
+
+    public static void main(String[] arg) {
+        testDynamic1();
+    }
+
+    private static void testDynamic1() {
+        logger.info("{}", backPackVariation(new int[]{1, 3, 4}, 10));
+    }
+
+    static int backPackVariation(int[] numbers, int n) {
+        // filter abnormal inputs
+        if (numbers == null || numbers.length == 0) {
+            return 1;
+        }
+
+        // populate the subproblem result array
+        int len = numbers.length;
+        int[] ret = new int[n + 1];
+        ret[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i - numbers[j] >= 0) {
+                    ret[i] += ret[i - numbers[j]];
+                }
+            }
+        }
+
+        // print to verify before commiting
+        System.out.println(Arrays.toString(ret));
+
+        // return result
+        return ret[n];
+    }
+}
