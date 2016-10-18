127516ee887cc86ba28399783d508d737f6d6b83
Yanliang Han <skywalkerhunter@gmail.com>
2016-10-18 15:15:05 -0400

    add lintCode. TwoSum, Two Pointers.

---
+++ src/main/java/com/lintCode/Advanced/TwoPointer/TwoSum.java
@@ -0,0 +1,87 @@
+package com.lintCode.Advanced.TwoPointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yanli on 2016-10-18.
+ */
+public class TwoSum {
+    private final static Logger logger = LoggerFactory.getLogger(TwoSum.class);
+
+    public static void main(String[] arg) {
+        new TwoSum().testTwoSum();
+    }
+
+    private void testTwoSum() {
+        logger.info("{}", twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        // handle extreme cases
+        if (numbers == null || numbers.length == 0) {
+            return new int[0];
+        }
+
+        int n = numbers.length;
+        ArrayList<Entry> entries = new ArrayList<Entry>(n);
+        for (int i = 0; i < numbers.length; i++) {
+            entries.add(new Entry(numbers[i], i));
+        }
+        int i = 0;
+        int j = n - 1;
+        Collections.sort(entries, entryComparator);
+        while (i < j) {
+            if (entries.get(i).val + entries.get(j).val < target) {
+                i++;
+            } else if (entries.get(i).val + entries.get(j).val > target) {
+                j--;
+            } else {
+                int[] ans = {entries.get(i).x + 1, entries.get(j).x + 1};
+                Arrays.sort(ans);
+                return ans;
+            }
+        }
+
+        return new int[0];
+    }
+
+    private Comparator<Entry> entryComparator = new Comparator<Entry>() {
+        public int compare(Entry o1, Entry o2) {
+            return o1.val - o2.val;
+        }
+    };
+
+    private class Entry {
+        int val;
+        int x;
+
+        public Entry(int val, int x) {
+            this.val = val;
+            this.x = x;
+        }
+
+        public Entry() {
+
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "val=" + val +
+                    ", x=" + x +
+                    '}';
+        }
+    }
+}
