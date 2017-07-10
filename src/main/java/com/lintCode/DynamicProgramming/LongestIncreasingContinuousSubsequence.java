a55dfe6892b19a70cf8d40b92e3e55934fe3d330
oliverwreath <+++hyl7713996>
2017-07-10 19:40:47 -0400

    dynamic programming. O(1) space, O(n) time. don't forget to do two passes.

--- src/main/java/com/lintCode/DynamicProgramming/LongestIncreasingContinuousSubsequence.java
+++ src/main/java/com/lintCode/DynamicProgramming/LongestIncreasingContinuousSubsequence.java
@@ -14,8 +14,8 @@ public class LongestIncreasingContinuousSubsequence {
     }
 
     private static void testLongestIncreasingContinuousSubsequence() {
-        logger.info("{}", longestIncreasingContinuousSubsequence(new int[]{5, 4, 2, 1, 3}));
-        logger.info("{}", longestIncreasingContinuousSubsequence(new int[]{5, 1, 2, 3, 4}));
+        logger.info("result {} = {}", 4, longestIncreasingContinuousSubsequence(new int[]{5, 4, 2, 1, 3}));
+        logger.info("result {} = {}", 4, longestIncreasingContinuousSubsequence(new int[]{5, 1, 2, 3, 4}));
     }
 
     /**
@@ -32,7 +32,8 @@ public class LongestIncreasingContinuousSubsequence {
         // fill the subproblem results
         int maxLength = 1;
         int length = 1;
-        for (int i = 1; i < A.length; i++) {
+        int n = A.length;
+        for (int i = 1; i < n; i++) {
             if (A[i - 1] < A[i]) {
                 length++;
                 maxLength = Math.max(maxLength, length);
@@ -40,8 +41,9 @@ public class LongestIncreasingContinuousSubsequence {
                 length = 1;
             }
         }
+
         length = 1;
-        for (int i = A.length - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {
             if (A[i] > A[i + 1]) {
                 length++;
                 maxLength = Math.max(maxLength, length);
@@ -53,4 +55,36 @@ public class LongestIncreasingContinuousSubsequence {
         // return the final result
         return maxLength;
     }
+
+//    static int longestIncreasingContinuousSubsequence(int[] A) {
+//        // Write your code here
+//        // filter abnormal inputs
+//        if (A == null || A.length == 0) {
+//            return 0;
+//        }
+//
+//        // fill the subproblem results
+//        int maxLength = 1;
+//        int length = 1;
+//        for (int i = 1; i < A.length; i++) {
+//            if (A[i - 1] < A[i]) {
+//                length++;
+//                maxLength = Math.max(maxLength, length);
+//            } else {
+//                length = 1;
+//            }
+//        }
+//        length = 1;
+//        for (int i = A.length - 2; i >= 0; i--) {
+//            if (A[i] > A[i + 1]) {
+//                length++;
+//                maxLength = Math.max(maxLength, length);
+//            } else {
+//                length = 1;
+//            }
+//        }
+//
+//        // return the final result
+//        return maxLength;
+//    }
 }
