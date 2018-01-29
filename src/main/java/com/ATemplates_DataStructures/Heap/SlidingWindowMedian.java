eba8a1c54812c8caff9456f92abb2793b0dbaf90
oliverwreath <+++hyl7713996>
2018-01-28 22:15:37 -0500

    heap - sliding window median.

--- src/main/java/com/ATemplates_DataStructures/Heap/SlidingWindowMedian.java
+++ src/main/java/com/ATemplates_DataStructures/Heap/SlidingWindowMedian.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -18,19 +17,11 @@ public class SlidingWindowMedian {
     }
 
     private void testMedianSlidingWindow() {
-        logger.info("2, 7, 7 = {}", medianSlidingWindow(new int[]{1, 2, 7, 8, 5}, 3));
+        logger.info("2, 7, 7 == {}", medianSlidingWindow(new int[]{1, 2, 7, 8, 5}, 3));
+        logger.info("1,2,7,2,2,3,3,4 == {}", medianSlidingWindow(new int[]{1, 2, 7, 7, 2, 10, 3, 4, 5}, 2));
 //        logger.info("4, 4, 4, 3, 3, 3, 3 = {}", medianSlidingWindow(new int[]{4, 5, 1, 3, 2, 6, 0}));
 //        logger.info("2, 2, 20 = {}", medianSlidingWindow(new int[]{2, 20, 100}));
     }
-
-    private PriorityQueue<Integer> maxHeap;
-    private PriorityQueue<Integer> minHeap;
-
-    private static Comparator<Integer> maxComparator = new Comparator<Integer>() {
-        public int compare(Integer o1, Integer o2) {
-            return o2 - o1;
-        }
-    };
 
     /**
      * @param nums: A list of integers.
@@ -38,54 +29,79 @@ public class SlidingWindowMedian {
      */
     public ArrayList<Integer> medianSlidingWindow(int[] nums, int k) {
         // write your code here
-        if (nums == null || nums.length == 0 || k < 1) {
-            return new ArrayList<Integer>();
+        if (nums == null || nums.length == 0 || nums.length < k) {
+            return new ArrayList<>();
         }
-        ArrayList<Integer> ans = new ArrayList<Integer>();
+        ArrayList<Integer> answer = new ArrayList<>();
         if (k == 1) {
             for (int num : nums) {
-                ans.add(num);
+                answer.add(num);
             }
-            return ans;
+            return answer;
         }
 
         // Heap
-        maxHeap = new PriorityQueue<Integer>(k, maxComparator);
-        minHeap = new PriorityQueue<Integer>(k);
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            // maxHeap <= minHeap
-            minHeap.add(nums[i]);
-            maxHeap.add(minHeap.poll());
-            // delete if too much
-            if (i >= k) {
-                if (!maxHeap.remove(nums[i - k])) {
-                    minHeap.remove(nums[i - k]);
-                }
-            }
-            // balance
-            while (minHeap.size() + 1 < maxHeap.size()) {
-                minHeap.add(maxHeap.poll());
-            }
-            while (maxHeap.size() + 1 < minHeap.size()) {
-                maxHeap.add(minHeap.poll());
-            }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, (o1, o2) -> o2 - o1);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        int len = nums.length;
+        for (int i = 0; i < k; i++) {
+            add(maxHeap, minHeap, nums[i]);
             if (i >= k - 1) {
-                ans.add(getMed(maxHeap, minHeap));
+                answer.add(maxHeap.peek());
+            }
+        }
+        for (int i = k; i < len; i++) {
+            deleteAdd(maxHeap, minHeap, nums[i], nums[i - k]);
+            if (i >= k - 1) {
+                answer.add(maxHeap.peek());
             }
         }
 
         // return
-        return ans;
+        return answer;
     }
 
-    private int getMed(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
-        if (maxHeap.size() == minHeap.size()) {
-            return maxHeap.peek();
-        } else if (minHeap.size() + 1 == maxHeap.size()) {
-            return maxHeap.peek();
+    private void add(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap, int input) {
+        if (maxHeap.size() <= minHeap.size()) {
+            if (minHeap.isEmpty() || minHeap.peek() > input) {
+                maxHeap.add(input);
+            } else {
+                maxHeap.add(minHeap.poll());
+                minHeap.add(input);
+            }
         } else {
-            return minHeap.peek();
+            if (maxHeap.isEmpty() || maxHeap.peek() < input) {
+                minHeap.add(input);
+            } else {
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(input);
+            }
+        }
+    }
+
+    private void deleteAdd(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap, int input, int target) {
+        if (target <= maxHeap.peek()) {
+            maxHeap.remove(target);
+        } else {
+            minHeap.remove(target);
+        }
+//        if (maxHeap.size() < minHeap.size()) {
+//            maxHeap.add(minHeap.poll());
+//        }
+        if (maxHeap.size() <= minHeap.size()) {
+            if (minHeap.isEmpty() || minHeap.peek() > input) {
+                maxHeap.add(input);
+            } else {
+                maxHeap.add(minHeap.poll());
+                minHeap.add(input);
+            }
+        } else {
+            if (maxHeap.isEmpty() || maxHeap.peek() < input) {
+                minHeap.add(input);
+            } else {
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(input);
+            }
         }
     }
 }
