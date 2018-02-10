732d1e52359b80cfa38e01704919085da2d0622b
oliverwreath <skywalkerhunter@gmail.com>
2018-02-10 17:41:54 -0500

    dp oh just to save some space. no big deal.

--- src/main/java/com/ATemplates_DataStructures/DP/No392_HouseRobber.java
+++ src/main/java/com/ATemplates_DataStructures/DP/No392_HouseRobber.java
@@ -15,10 +15,6 @@ public class No392_HouseRobber {
         MyLogger.info("result 8 v.s. " + No392_HouseRobber.houseRobber(new int[]{3, 8, 4}));
     }
 
-    /**
-     * @param A an array of Integer
-     * @return an integer
-     */
     public long houseRobber(int[] A) {
         // filter abnormal cases
         if (A == null || A.length == 0) {
@@ -29,16 +25,37 @@ public class No392_HouseRobber {
         }
 
         int len = A.length;
-        long[] dp = new long[len];
+        long[] dp = new long[2];
         dp[0] = A[0];
         dp[1] = Math.max(A[0], A[1]);
         for (int i = 2; i < len; i++) {
-            dp[i] = Math.max(dp[i - 1], A[i] + dp[i - 2]);
+            dp[i % 2] = Math.max(dp[(i - 1) % 2], A[i] + dp[(i - 2) % 2]);
         }
 
         // return the final result
-        return dp[len - 1];
+        return dp[(len - 1) % 2];
     }
+
+//    public long houseRobber(int[] A) {
+//        // filter abnormal cases
+//        if (A == null || A.length == 0) {
+//            return 0;
+//        }
+//        if (A.length == 1) {
+//            return A[0];
+//        }
+//
+//        int len = A.length;
+//        long[] dp = new long[len];
+//        dp[0] = A[0];
+//        dp[1] = Math.max(A[0], A[1]);
+//        for (int i = 2; i < len; i++) {
+//            dp[i] = Math.max(dp[i - 1], A[i] + dp[i - 2]);
+//        }
+//
+//        // return the final result
+//        return dp[len - 1];
+//    }
 
     private static class MyLogger {
         static boolean isDebugging = false;
