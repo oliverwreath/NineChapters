482a9c335f64fadd255e67041f8a5d8ae5c941e7
oliverwreath <skywalkerhunter@gmail.com>
2018-02-10 18:35:59 -0500

    compress some space.

--- src/main/java/com/ATemplates_DataStructures/DP/No114_UniquePaths.java
+++ src/main/java/com/ATemplates_DataStructures/DP/No114_UniquePaths.java
@@ -22,19 +22,19 @@ public class No114_UniquePaths {
             return 0;
         }
 
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[2][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
+                    dp[i % 2][j] = 1;
                 } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[i % 2][j - 1];
                 }
             }
         }
 
         // return the final result
-        return dp[m - 1][n - 1];
+        return dp[(m - 1) % 2][n - 1];
     }
 
 //    public long houseRobber(int[] A) {
