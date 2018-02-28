3129668d618af85d8987c41f004a7827fca2b852
oliverwreath <skywalkerhunter@gmail.com>
2018-02-27 22:52:19 -0500

    hell of a DP, hard as hell.

---
+++ src/main/java/com/ATemplates_DataStructures/DP/No192_WildcardMatching.java
@@ -0,0 +1,85 @@
+package com.ATemplates_DataStructures.DP;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class No192_WildcardMatching {
+    private final static Logger logger = LoggerFactory.getLogger(No192_WildcardMatching.class);
+
+    public static void main(String[] args) {
+        No192_WildcardMatching No192_WildcardMatching = new No192_WildcardMatching();
+        No192_WildcardMatching.testNo192_WildcardMatching();
+    }
+
+    private void testNo192_WildcardMatching() {
+        MyLogger.info("result false v.s. " + isMatch("aaa", "aa"));
+        MyLogger.info("result true v.s. " + isMatch("aa", "a*"));
+        MyLogger.info("result false v.s. " + isMatch("aab", "c*a*b"));
+    }
+
+    boolean isMatch(String S, String P) {
+        // filter abnormal cases
+        if (S == null || S.length() == 0) {
+            return isEmpty(P);
+        }
+        if (P == null || P.length() == 0) {
+            return false;
+        }
+
+        int m = S.length();
+        int n = P.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (j == 0) {
+                    dp[i][j] = false;
+                } else {
+                    if (P.charAt(j - 1) == '*') {
+                        dp[i][j] |= dp[i][j - 1];
+                        if (i - 1 >= 0) {
+                            dp[i][j] |= dp[i - 1][j];
+                        }
+                    } else {
+                        if (i > 0 && (P.charAt(j - 1) == '?' || S.charAt(i - 1) == P.charAt(j - 1))) {
+                            dp[i][j] |= dp[i - 1][j - 1];
+                        }
+                    }
+                }
+            }
+        }
+
+        // return the final result
+        return dp[m][n];
+    }
+
+    private boolean isEmpty(String p) {
+        if (p == null || p.length() == 0) {
+            return true;
+        }
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static class MyLogger {
+        static boolean isDebugging = false;
+        static boolean isInfoing = true;
+
+        static void debug(Object message) {
+            if (isDebugging) {
+                System.out.println("MyLogger.Debugging = " + message);
+            }
+        }
+
+        static void info(Object message) {
+            if (isInfoing) {
+                System.out.println("MyLogger.Debugging = " + message);
+            }
+        }
+    }
+}
