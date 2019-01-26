0e4eb229ff3fe7a194dcc273b95494bbdae2109b
oliverwreath <skywalkerhunter@gmail.com>
2019-01-26 18:47:04 -0500

    Two pointers. converging.

---
+++ src/main/java/com/NineChapters/TwoPointers/Reverse/ValidPalindrome_415.java
@@ -0,0 +1,66 @@
+package com.NineChapters.TwoPointers.Reverse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ValidPalindrome_415 {
+    private final static Logger logger = LoggerFactory.getLogger(ValidPalindrome_415.class);
+
+    public static void main(String[] args) {
+        testValidPalindrome_415();
+    }
+
+    private static void testValidPalindrome_415() {
+        logger.info("result true v.s. {}", isPalindrome("A man, a plan, a canal: Panama"));
+        logger.info("result false v.s. {}", isPalindrome("race a car"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        // filter abnormal cases
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isDigit(s.charAt(left)) && !Character.isAlphabetic(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isDigit(s.charAt(right)) && !Character.isAlphabetic(s.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            } else {
+                left++;
+                right--;
+                continue;
+            }
+        }
+
+        // return the final result
+        return true;
+    }
+
+    private static class MyLogger {
+        private static final boolean isDebugging = false;
+        private static final boolean isInfoing = true;
+        private static final String DEBUG = "[DEBUG]";
+        private static final String INFO = "[INFO]";
+
+        static void debug(Object message) {
+            if (isDebugging) {
+                System.out.println(DEBUG + " = " + message);
+            }
+        }
+
+        static void info(Object message) {
+            if (isInfoing) {
+                System.out.println(INFO + " = " + message);
+            }
+        }
+    }
+}
