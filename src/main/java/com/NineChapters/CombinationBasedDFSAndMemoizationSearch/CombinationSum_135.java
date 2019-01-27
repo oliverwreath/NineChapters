4659c9a49622f926649612f3e84e903741435e1c
oliverwreath <skywalkerhunter@gmail.com>
2019-01-27 17:16:12 -0500

    quite tricky DFS for combinations. will revisit later.

---
+++ src/main/java/com/NineChapters/CombinationBasedDFSAndMemoizationSearch/CombinationSum_135.java
@@ -0,0 +1,79 @@
+package com.NineChapters.CombinationBasedDFSAndMemoizationSearch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum_135 {
+    private final static Logger logger = LoggerFactory.getLogger(CombinationSum_135.class);
+
+    public static void main(String[] args) {
+        testCombinationSum_135();
+    }
+
+    private static void testCombinationSum_135() {
+        logger.info("result [[7],[2,2,3]] v.s. {}", combinationSum(new int[]{2, 3, 6, 7}, 7));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // filter abnormal cases
+        if (candidates == null || candidates.length == 0) {
+            return new LinkedList<>();
+        }
+
+        Arrays.sort(candidates);
+        List<List<Integer>> results = new ArrayList<>();
+        dfs(candidates, target, 0, new ArrayList<>(), results);
+
+        // return the final result
+        return results;
+    }
+
+    private static void dfs(int[] candidates, int target, int index, List<Integer> combination, List<List<Integer>> results) {
+        if (index == candidates.length) {
+            if (target == 0) {
+                results.add(new ArrayList<>(combination));
+            }
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        // skip the candidate
+        dfs(candidates, target, index + 1, combination, results);
+
+        if (index > 0 && candidates[index] == candidates[index - 1]) {
+            return;
+        }
+
+        // recruit the candidate
+        combination.add(candidates[index]);
+        dfs(candidates, target - candidates[index], index, combination, results);
+        combination.remove(combination.size() - 1);
+    }
+
+    private static class MyLogger {
+        private static final boolean isDebugging = false;
+        private static final boolean isInfoing = true;
+        private static final String DEBUG = "[DEBUG]";
+        private static final String INFO = "[INFO]";
+
+        static void debug(Object message) {
+            if (isDebugging) {
+                System.out.println(DEBUG + " = " + message);
+            }
+        }
+
+        static void info(Object message) {
+            if (isInfoing) {
+                System.out.println(INFO + " = " + message);
+            }
+        }
+    }
+}
