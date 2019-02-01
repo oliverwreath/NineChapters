216157d9f07ccffc8149793d500ec2b64d183f57
oliverwreath <skywalkerhunter@gmail.com>
2019-02-01 15:57:01 -0500

    /**  * version 1 - nicely done! it's working!  * version 2 - nicely done! One dp array.  * version 3 - nicely done! Sliding Array.  */

--- src/main/java/com/AdvancedAlgorithms/DPI/MaximalSquare_436.java
+++ src/main/java/com/AdvancedAlgorithms/DPI/MaximalSquare_436.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 /**
  * version 1 - nicely done! it's working!
  * version 2 - nicely done! One dp array.
+ * version 3 - nicely done! Sliding Array.
  */
 public class MaximalSquare_436 {
     private final static Logger logger = LoggerFactory.getLogger(MaximalSquare_436.class);
@@ -35,31 +36,34 @@ public class MaximalSquare_436 {
         }
 
         int m = matrix.length, n = matrix[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = matrix[0][0];
+        int[][] dp = new int[2][n];
+        int max = dp[0][0] = matrix[0][0];
         for (int j = 1; j < n; j++) {
             dp[0][j] = matrix[0][j] == 1 ? 1 : 0;
+            max = Math.max(max, dp[0][j]);
         }
         for (int i = 1; i < m; i++) {
-            dp[i][0] = matrix[i][0] == 1 ? 1 : 0;
+            dp[i%2][0] = matrix[i][0] == 1 ? 1 : 0;
+            max = Math.max(max, dp[i%2][0]);
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 1) {
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                    dp[i%2][j] = 1 + Math.min(Math.min(dp[(i - 1)%2][j - 1], dp[(i - 1)%2][j]), dp[i%2][j - 1]);
                 } else {
-                    dp[i][j] = 0;
+                    dp[i%2][j] = 0;
                 }
+                max = Math.max(max, dp[i%2][j]);
             }
         }
-        for (int i = 0; i < m; i++) {
-            MyLogger.info(Arrays.toString(dp[i]));
-        }
+//        for (int i = 0; i < m; i++) {
+//            MyLogger.info(Arrays.toString(dp[i]));
+//        }
 
-        int max = dp[0][0];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, dp[i][j]);
-            }
-        }
+//        int max = dp[0][0];
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                max = Math.max(max, dp[i][j]);
+//            }
+//        }
 
         // return the final result
         return max * max;
